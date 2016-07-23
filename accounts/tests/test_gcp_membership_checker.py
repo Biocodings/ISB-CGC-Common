72e71d36dc7221a641ed2df6994ebac6d8ef14eb
kleinone <kleinone@systemsbiology.org>
2016-07-22 19:14:05 -0700

    Updated and added tests

--- accounts/tests/test_gcp_membership_checker.py
+++ accounts/tests/test_gcp_membership_checker.py
@@ -23,7 +23,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from accounts.models import AuthorizedDataset, NIH_User, GoogleProject, ServiceAccount, UserAuthorizedDatasets
 from tasks.nih_whitelist_processor.utils import DatasetToACLMapping
-from tasks.nih_whitelist_processor.gcp_utils import GoogleProjectMembershipChecker
+from tasks.nih_whitelist_processor.gcp_utils import GoogleProjectMembershipChecker, GoogleProjectRemoveEntry
 
 logging.basicConfig(
     level=logging.INFO
@@ -83,7 +83,7 @@ class TestPublicDatasetServiceAccount(TestCase):
         self.assertEquals(len(result.skipped_service_accounts), 1)
 
         # No project-specific actions should have been generated
-        self.assertDictEqual(result.projects, {})
+        self.assertEquals(len(result.acl_remove_list), 0)
 
 
 class TestUnauthorizedUser(TestCase):
@@ -142,6 +142,89 @@ class TestUnauthorizedUser(TestCase):
         # The service account should not have been skipped, as it is linked to a protected dataset
         self.assertEquals(len(result.skipped_service_accounts), 0)
 
-        self.assertEquals(result.projects[self.project_123.pk].acl_remove_list[0], ('acl-phs000123', 'abc_123'))
+        self.assertEquals(len(result.acl_remove_list), 1)
+        self.assertEquals(result.acl_remove_list[0],
+                          GoogleProjectRemoveEntry.from_models(self.project_123, self.account_123, 'acl-phs000123'))
 
+
+class TestPublicAndProtectedDataset(TestCase):
+    def setUp(self):
+        test_dataset_mapping = {
+            'phs000123': {
+                'name': 'This is a study',
+                'parent_study': 'phs000111',
+                'acl_group': 'acl-phs000123'
+            },
+            'phs000456': {
+                'name': 'Another study',
+                'parent_study': 'phs000444',
+                'acl_group': 'acl-phs000456'
+            }
+        }
+
+        self.dataset_acl_mapping = DatasetToACLMapping(test_dataset_mapping)
+
+        self.auth_user = User(first_name='Test', last_name='User', username='test_user', email='test@email.com')
+        self.auth_user.save()
+
+        self.nih_user = NIH_User(user=self.auth_user,
+                                 NIH_username='USERNAME1',
+                                 NIH_assertion='012345689',
+                                 dbGaP_authorized=True,
+                                 active=True)
+
+        self.nih_user.save()
+
+        self.auth_dataset_123 = AuthorizedDataset(name="dataset1", whitelist_id='phs000123',
+                                                  acl_google_group='acl-phs000123', public=True)
+        self.auth_dataset_123.save()
+
+        self.project_123 = GoogleProject(project_name="project1",
+                                         project_id="123",
+                                         big_query_dataset="bq_dataset1")
+        self.project_123.save()
+        self.project_123.user.add(self.auth_user)
+
+        self.account_123 = ServiceAccount(google_project=self.project_123, service_account="abc_123",
+                                          authorized_dataset=self.auth_dataset_123)
+        self.account_123.save()
+
+        self.auth_dataset_456 = AuthorizedDataset(name="dataset1", whitelist_id='phs000456',
+                                                  acl_google_group='acl-phs000456', public=False)
+        self.auth_dataset_456.save()
+
+        self.account_456 = ServiceAccount(google_project=self.project_123, service_account="456@institution.org",
+                                          authorized_dataset=self.auth_dataset_456)
+        self.account_456.save()
+
+    def test_both_accounts_authorized(self):
+        uad_123 = UserAuthorizedDatasets(nih_user=self.nih_user, authorized_dataset=self.auth_dataset_123)
+        uad_123.save()
+
+        uad_456 = UserAuthorizedDatasets(nih_user=self.nih_user, authorized_dataset=self.auth_dataset_456)
+        uad_456.save()
+
+        gmc = GoogleProjectMembershipChecker(self.dataset_acl_mapping)
+        result = gmc.process()
+
+        # Account 'abc_123' should have been skipped, as it is associated to a public dataset
+        self.assertEquals(len(result.skipped_service_accounts), 1)
+        self.assertEquals(result.skipped_service_accounts[0], "abc_123")
+
+        self.assertEquals(len(result.acl_remove_list), 0)
+
+    def test_protected_account_not_authorized(self):
+        uad_123 = UserAuthorizedDatasets(nih_user=self.nih_user, authorized_dataset=self.auth_dataset_123)
+        uad_123.save()
+
+        gmc = GoogleProjectMembershipChecker(self.dataset_acl_mapping)
+        result = gmc.process()
+
+        self.assertEquals(len(result.skipped_service_accounts), 1)
+        self.assertEquals(result.skipped_service_accounts[0], "abc_123")
+
+        # Only 'abc_456' should have been marked for removal
+        self.assertEquals(len(result.acl_remove_list), 1)
+        self.assertEquals(result.acl_remove_list[0],
+                          GoogleProjectRemoveEntry.from_models(self.project_123, self.account_456, 'acl-phs000456'))
 
